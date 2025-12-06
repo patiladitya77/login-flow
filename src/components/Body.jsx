388890500960a83cea6c927abd47ef09e1291664
patiladitya77 <adpatil587@gmail.com>
2025-12-07 00:03:03 +0530

    setting up routing

---
+++ src/components/Body.jsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./Login";
+import Dashboard from "./Dashboard";
+
+const Body = () => {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/dashboard",
+      element: <Dashboard />,
+    },
+  ]);
+
+  return (
+    <div>
+      <RouterProvider router={appRouter} />
+    </div>
+  );
+};
+
+export default Body;
